9cd533207af74063009224edd6fc8c8336aa0401
iCaptainNemo <60410928+iCaptainNemo@users.noreply.github.com>
2023-12-02 22:37:41 -0800

    Update helpdesk_jarvis.ps1
    Revised while in environment.
     Added color coding and proper loop.

--- helpdesk_jarvis.ps1
+++ helpdesk_jarvis.ps1
@@ -1,15 +1,16 @@
+Clear-Host
 # Import the Active Directory module
 Import-Module ActiveDirectory
 
 # Prompt for temporary password or default to the current season and year
-$temporaryPassword = Read-Host "Enter the temporary password (default is $(Get-Date -UFormat '%B%Y'))"
+$temporaryPassword = Read-Host "Enter a temporary password to use for password resets or leave as default ($(Get-Date -UFormat '%B%Y'))"
 if (-not $temporaryPassword) {
     $temporaryPassword = Get-Date -UFormat '%B%Y'
 }
 
 # Function to get User ID
 function Get-UserId {
-    $userId = Read-Host "Enter User ID"
+    $userId = Read-Host "Enter New User ID"
     return $userId
 }
 
@@ -28,35 +29,63 @@ function Get-ADUserProperties {
     }
 }
 
-# Function to display AD properties
+# Function to display AD properties as a table with color coding
 function Show-ADUserProperties {
     param (
         [Microsoft.ActiveDirectory.Management.ADUser]$adUser
     )
 
     if ($adUser) {
-        Write-Host "User ID: $($adUser.SamAccountName)"
-        Write-Host "Display Name: $($adUser.DisplayName)"
-        Write-Host "Email: $($adUser.EmailAddress)"
-        Write-Host "Description: $($adUser.Description)"
-
-        # Color coding for lockout status
-        $lockoutStatus = $adUser.LockoutEnabled
-        if ($lockoutStatus) {
-            Write-Host "Lockout Status: $($adUser.LockoutEnabled)" -ForegroundColor Red
-        } else {
-            Write-Host "Lockout Status: $($adUser.LockoutEnabled)" -ForegroundColor Green
+        $properties = [ordered]@{
+            'User ID'                   = $adUser.SamAccountName
+            'Given Name'                = $adUser.GivenName
+            'Display Name'              = $adUser.DisplayName
+            'Email'                     = $adUser.EmailAddress
+            'Department'                = $adUser.Department
+            'Password Last Set'         = $adUser.PasswordLastSet
+            'Account Lockout Time'      = $adUser.AccountLockoutTime
+            'Last Bad Password Attempt' = $adUser.LastBadPasswordAttempt
+            'Bad Logon Count'           = $adUser.BadLogonCount
+            'Bad Password Count'        = $adUser.badPwdCount
         }
+        # Display properties with color coding
+        $properties.GetEnumerator() | Format-Table
 
-        # Color coding for password expiration status
+        # Color coding for Password Expired
         $passwordExpired = $adUser.PasswordExpired
         if ($passwordExpired) {
-            Write-Host "Password Expired: $($adUser.PasswordExpired)" -ForegroundColor Red
+            Write-Host "Password Expired: Expired" -ForegroundColor Red
         } else {
-            Write-Host "Password Expired: $($adUser.PasswordExpired)" -ForegroundColor Green
+            Write-Host "Password Expired: Not Expired" -ForegroundColor Green
         }
 
-        # Add more properties as needed
+        # Color coding for LockedOut
+        $lockedOut = $adUser.LockedOut
+        if ($lockedOut) {
+            Write-Host "LockedOut: True" -ForegroundColor Red
+        } else {
+            Write-Host "LockedOut: False" -ForegroundColor Green
+        }
+
+    }
+}
+
+
+
+# Function to display last 10 log entries
+function Show-LastLogEntries {
+    param (
+        [string]$logFilePath
+    )
+
+    try {
+        $logEntries = Get-Content $logFilePath -Tail 10
+        Write-Host "Last 10 log entries:"
+        foreach ($entry in $logEntries) {
+            Write-Host $entry
+        }
+    } catch {
+        Write-Host "Error: $_"
     }
 }
 
@@ -78,6 +107,7 @@ function Unlock-ADAccountOnAllDomainControllers {
     }
 }
 
+
 # Main loop
 while ($true) {
     # Get User ID before entering the main menu
@@ -91,11 +121,22 @@ while ($true) {
         # Display AD properties above the menu
         Show-ADUserProperties -adUser $adUser
 
+        # Add a line break or additional Write-Host statements for space
+        Write-Host "`n"  # This adds a line break
+
+        # Display last 10 log entries
+        $logFilePath = "\\hssserver037\login-tracking\$userId.log"
+        Show-LastLogEntries -logFilePath $logFilePath
+
+        # Add a line break or additional Write-Host statements for space
+        Write-Host "`n"  # This adds a line break
+
         # Main menu loop
-        Clear-Host
         Write-Host "1. Unlock AD Account on All Domain Controllers"
         Write-Host "2. Set Temporary Password (User Must Change)"
-        Write-Host "3. Clear and Restart"
+        Write-Host "3. Display AD User Properties"
+        Write-Host "4. Show Last 10 Log Entries"
+        Write-Host "5. Clear and Restart"
 
         $choice = Read-Host "Enter your choice"
 
@@ -119,8 +160,22 @@ while ($true) {
                 Read-Host
             }
             '3' {
-                # Clear the console and restart the script
+                # Display AD properties for the provided User ID
+                Show-ADUserProperties -adUser $adUser
+                Write-Host "Press Enter to continue"
+                Read-Host
+            }
+            '4' {
+                # Show last 10 log entries
+                Show-LastLogEntries -logFilePath $logFilePath
+                Write-Host "Press Enter to continue"
+                Read-Host
+            }
+            '5' {
+                # Clear the console, reset User ID, and restart the script
+                $userId = $null
                 Clear-Host
+                $userId = Read-Host "Enter New User ID"
                 break
             }
         }
