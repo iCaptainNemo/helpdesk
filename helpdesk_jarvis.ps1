f98f76b8de76269b441c9036691cdc3c89fd5171
iCaptainNemo <60410928+iCaptainNemo@users.noreply.github.com>
2023-12-12 22:54:38 -0800

    Update helpdesk_jarvis.ps1
    production improvements with submenu and error handling, code simplification.

--- helpdesk_jarvis.ps1
+++ helpdesk_jarvis.ps1
@@ -1,7 +1,3 @@
-Clear-Host
-# Import the Active Directory module
-Import-Module ActiveDirectory
-
 # Function to retrieve domain controllers
 function Get-DomainControllers {
     return Get-ADDomainController -Filter *
@@ -15,12 +11,13 @@ function Get-UserId {
             Get-ADUser -Identity $userId -ErrorAction Stop | Out-Null
             return $userId
         } catch {
-            Write-Host "Cannot find an object with given identity. Try again."
+            #Clear-Host
+            Write-Host "Cannot find an object with the given identity. Try again."
         }
     }
 }
 
-# Function to get AD properties for a given User ID
+# Function to get specific AD properties for a given User ID
 function Get-ADUserProperties {
     param (
         [string]$userId
@@ -70,7 +67,7 @@ function Show-ADUserProperties {
         if ($passwordLastSet -ne $null) {
             $daysSinceLastSet = (Get-Date) - $passwordLastSet
             if ($daysSinceLastSet.TotalDays -le 14) {
-                Write-Host "Password Last Set: $($passwordLastSet.ToString('yyyy-MM-dd HH:mm:ss')) (within last 14 days)" -ForegroundColor Green
+                Write-Host "Password Last Set: $($passwordLastSet.ToString('yyyy-MM-dd HH:mm:ss')) (within the last 14 days)" -ForegroundColor Green
             } else {
                 Write-Host "Password Last Set: $($passwordLastSet.ToString('yyyy-MM-dd HH:mm:ss')) (more than 14 days ago)" -ForegroundColor Yellow
             }
@@ -85,7 +82,6 @@ function Show-ADUserProperties {
         } else {
             Write-Host "LockedOut: False" -ForegroundColor Green
         }
-
     }
 }
 
@@ -102,33 +98,186 @@ function Show-LastLogEntries {
             Write-Host $entry
         }
     } catch {
-        $errorMessage = $_.Exception.Message
-
-        if ($errorMessage -like '*ObjectNotFound*') {
-            Write-Host "Error: Log file not found at $logFilePath" -ForegroundColor Yellow
-        } else {
-            Write-Host "Error: $errorMessage" -ForegroundColor Red
-        }
+        Write-Host "Error: $_" -ForegroundColor Red
+        Write-Host "No logs found" -ForegroundColor Yellow
     }
 }
 
 
 
-# Function to unlock an AD account on all domain controllers
-function Unlock-ADAccountOnAllDomainControllers {
+# Function to unlock an AD account on all reachable domain controllers
+function Unlock-ADAccountOnReachableDomainControllers {
     param (
         [string]$userId
     )
 
-    try {
-        $domainControllers = Get-DomainControllers
-        foreach ($dc in $domainControllers) {
-            Write-Host "Unlocking AD Account for User ID: $userId on Domain Controller: $($dc.Name)"
-            Unlock-ADAccount -Identity $userId -Server $dc.HostName -ErrorAction Stop
-            Write-Host "Account successfully unlocked on $($dc.Name)!"
+    $DCList = Get-DomainControllers
+
+    foreach ($targetDC in $DCList.Name) {
+        try {
+            Unlock-ADAccount -Identity $userId -Server $targetDC -ErrorAction Stop
+            Write-Host ("Unlocked in " + $targetDC) -BackgroundColor DarkGreen
+        } catch {
+            $errormsg = "Failed to unlock $userId in $targetDC. Error: $_"
+            Write-Host $errormsg -ForegroundColor White -BackgroundColor Red
         }
+    }
+}
+
+# Function to test connection to an asset
+function Test-AssetConnection {
+    param (
+        [string]$assetName
+    )
+
+    try {
+        $null = Test-Connection -ComputerName $assetName -Count 1 -ErrorAction Stop
+        return $true
     } catch {
-        Write-Host "Error: $_"
+        return $false
+    }
+}
+
+# Function to perform Asset Control actions
+function Asset-Control {
+    param (
+        [string]$userId
+    )
+
+    # Add a line break or additional Write-Host statements for space
+    Write-Host "`n"  # This adds a line break
+
+    # Prompt for Computer Name
+    $computerName = Read-Host "Enter Computer Name"
+
+    # Add a line break or additional Write-Host statements for space
+    Write-Host "`n"  # This adds a line break
+
+# Get computer properties
+try {
+    $computer = Get-ADComputer $computerName -Properties MemberOf
+    if ($computer) {
+        Write-Host "Computer Properties for $($computerName):"
+
+        $memberOf = $computer.MemberOf -join ', '
+
+        # Check if the required groups are present in MemberOf
+        $isHSRemoteComputers = $memberOf -like '*HSRemoteComputers*'
+        $isHSRemoteMFAComputers = $memberOf -like '*HSRemoteMFAComputers*'
+
+        # Display properties in a table
+        $properties = @{
+            'HSRemoteComputers'      = $isHSRemoteComputers
+            'HSRemoteMFAComputers'   = $isHSRemoteMFAComputers
+            'Computer Reachable' = Test-Connection -Count 1 -ComputerName $computerName -Quiet
+        }
+
+        $properties.GetEnumerator() | Format-Table
+    } else {
+        Write-Host "Computer not found: $computerName" -ForegroundColor Red
+        return
+    }
+} catch {
+    Write-Host "Error retrieving computer properties: $_" -ForegroundColor Red
+    return
+}
+
+# Submenu for Asset Control
+while ($true) {
+    Write-Host "`nAsset Control Menu"
+    Write-Host "1. Remote Desktop"
+    Write-Host "2. Remote Assistance"
+    Write-Host "3. Clear Browser Data"
+    Write-Host "4. Add Network Printer"
+    Write-Host "5. Back to Main Menu"
+
+    $assetChoice = Read-Host "Enter your choice"
+
+    switch ($assetChoice) {
+        '1' {
+            # Check if the SCCM remote tool executable exists
+            $sccmToolPath = "C:\Program Files (x86)\Microsoft Endpoint Manager\AdminConsole\bin\i386\CmRcViewer.exe"
+
+            if (Test-Path $sccmToolPath) {
+                try {
+                    # Invoke SCCM remote tool
+                    Start-Process -FilePath $sccmToolPath $computerName -Wait
+                    Write-Host "Remote Desktop launched for $computerName"
+                } catch {
+                    Write-Host "Error launching SCCM Remote Tool: $_" -ForegroundColor Red
+                }
+            } else {
+                Write-Host "SCCM Remote Tool not found at $sccmToolPath" -ForegroundColor Red
+            }
+        }
+        '2' {
+            # Launch Remote Assistance tool
+            # Add your Remote Assistance tool invocation command here
+            Write-Host "Launching Remote Assistance for $computerName"
+        }
+        '3' {
+            # Clear browser data
+            # Add your browser data clearing command here
+            Write-Host "Clearing Browser Data for $computerName"
+        }
+        '4' {
+            # Add network printer
+            $printServer = Read-Host "Enter Print Server Name"
+            $printerName = Read-Host "Enter Printer Name"
+            Add-NetworkPrinter -printServer $printServer -printerName $printerName
+        }
+        '5' {
+            # Back to main menu
+            return
+        }
+        default {
+            Write-Host "Invalid choice. Please enter a valid option."
+        }
+    }
+}
+
+}
+
+# Function to invoke SCCM remote tool
+function Invoke-SCCMRemoteTool {
+    param (
+        [string]$computerName
+    )
+
+    # Check if the SCCM remote tool executable exists
+    $sccmToolPath = "C:\Program Files (x86)\Microsoft Endpoint Manager\AdminConsole\bin\i386CmRcViewer.exe"
+
+    if (Test-Path $sccmToolPath) {
+        try {
+            
+            # Add a line break or additional Write-Host statements for space
+            Write-Host "`n"  # This adds a line break
+
+            # Invoke SCCM remote tool
+            Start-Process -FilePath $sccmToolPath -ArgumentList "/server:$computerName" -Wait
+            Write-Host "Launched SCCM Remote Tool for $computerName"
+        } catch {
+            Write-Host "Error launching SCCM Remote Tool: $_" -ForegroundColor Red
+        }
+    } else {
+        Write-Host "SCCM Remote Tool not found at $sccmToolPath" -ForegroundColor Red
+    }
+}
+
+# Function to add a network printer
+function Add-NetworkPrinter {
+    param (
+        [string]$printServer,
+        [string]$printerName
+    )
+
+    try {
+        # Add the network printer using the Add-Printer cmdlet
+        Add-Printer -ConnectionName "\\$printServer\$printerName"
+
+        Write-Host "Network printer '$printerName' added successfully from print server '$printServer'" -ForegroundColor Green
+    } catch {
+        Write-Host "Error adding network printer: $_" -ForegroundColor Red
     }
 }
 
@@ -136,6 +285,9 @@ function Unlock-ADAccountOnAllDomainControllers {
 while ($true) {
     # Get User ID before entering the main menu
     $userId = Get-UserId
+
+    # Initialize $logFilePath inside the main loop
+    $logFilePath = "\\hssserver037\login-tracking\$userId.log"
 
     # Loop for the main menu
     while ($true) {
@@ -149,24 +301,19 @@ while ($true) {
         Write-Host "`n"  # This adds a line break
 
         # Display last 10 log entries
-        $logFilePath = "\\hssserver037\login-tracking\$userId.log"
         Show-LastLogEntries -logFilePath $logFilePath
 
         # Add a line break or additional Write-Host statements for space
         Write-Host "`n"  # This adds a line break
 
         # Main menu loop
-        Write-Host "1. Clear and Restart"
+        Write-Host "1. Clear and Restart Script"
         Write-Host "2. Unlock AD Account on All Domain Controllers"
-        Write-Host "3. Set Temporary Password (User Must Change)"
-        Write-Host "4. Quit"
+        Write-Host "3. Password Reset"
+        Write-Host "4. Asset Control"
+        Write-Host "5. Quit"
 
         $choice = Read-Host "Enter your choice"
-
-        if ($choice -eq '') {
-            # If Enter is pressed without entering a number, restart from Show-ADUserProperties
-            continue
-        }
 
         switch ($choice) {
             '1' {
@@ -174,43 +321,70 @@ while ($true) {
                 $userId = $null
                 Clear-Host
                 $userId = Get-UserId
+                # Reset log file path
+                $logFilePath = "\\hssserver037\login-tracking\$userId.log"
                 break
             }
             '2' {
                 # Unlock AD account on all domain controllers
-                Unlock-ADAccountOnAllDomainControllers -userId $userId
+                Unlock-ADAccountOnReachableDomainControllers -userId $userId
                 Write-Host "Press Enter to continue"
                 Read-Host
             }
             '3' {
-                # Prompt for a custom temporary password or default to the current season and year
-                $customTemporaryPassword = Read-Host "Enter a custom temporary password for password resets (press Enter to use default based on SeasonYear)"
-                if (-not $customTemporaryPassword) {
-                    $currentMonth = (Get-Date).Month
-                    $season = switch ($currentMonth) {
-                        { $_ -in 3..5 } { 'Spring' }
-                        { $_ -in 6..8 } { 'Summer' }
-                        { $_ -in 9..11 } { 'Fall' }
-                        { $_ -in 1, 2, 12 } { 'Winter' }
-                    }
+                # Prompt for setting a temporary or permanent password
+                $passwordChoice = Read-Host "Do you want to set a temporary (T) or permanent (P) password? Enter T or P"
 
-                    # Set Temporary Password based on the season and year
-                    $temporaryPassword = "$season$(Get-Date -UFormat '%Y')"
-                } else {
-                    $temporaryPassword = $customTemporaryPassword
+                switch ($passwordChoice) {
+                    'T' {
+                        # Set Temporary Password based on the season and year
+                        $currentMonth = (Get-Date).Month
+                        $season = switch ($currentMonth) {
+                            { $_ -in 3..5 } { 'Spring' }
+                            { $_ -in 6..8 } { 'Summer' }
+                            { $_ -in 9..11 } { 'Fall' }
+                            { $_ -in 1, 2, 12 } { 'Winter' }
+                        }
+
+                        $temporaryPassword = "$season$(Get-Date -UFormat '%Y')"
+                        Write-Host "Setting Temporary Password for User ID: $userId to $temporaryPassword (User Must Change)"
+                        try {
+                            Set-ADAccountPassword -Identity $userId -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $temporaryPassword -Force) -ErrorAction Stop
+                            Set-ADUser -Identity $userId -ChangePasswordAtLogon $true -ErrorAction Stop
+                            Write-Host "Temporary password set to $temporaryPassword. User must change the password at the next login."
+                        } catch {
+                            Write-Host "Error: $_"
+                        }
+                        Write-Host "Press Enter to continue"
+                        Read-Host
+                        break
+                    }
+                    'P' {
+                        # Prompt for a permanent password
+                        $permanentPassword = Read-Host "Enter the permanent password for User ID: $userId"
+                        Write-Host "Setting Permanent Password for User ID: $userId"
+                        try {
+                            Set-ADAccountPassword -Identity $userId -NewPassword (ConvertTo-SecureString -AsPlainText $permanentPassword -Force) -ErrorAction Stop
+                            Set-ADUser -Identity $userId -ChangePasswordAtLogon $false -ErrorAction Stop
+                            Write-Host "Permanent password set for User ID: $userId"
+                        } catch {
+                            Write-Host "Error: $_"
+                        }
+                        Write-Host "Press Enter to continue"
+                        Read-Host
+                        break
+                    }
+                    default {
+                        Write-Host "Invalid choice. Please enter either T or P."
+                        break
+                    }
                 }
-                Write-Host "Setting Temporary Password for User ID: $userId to $temporaryPassword (User Must Change)"
-                try {
-                    Set-ADAccountPassword -Identity $userId -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $temporaryPassword -Force) -ErrorAction Stop
-                    Set-ADUser -Identity $userId -ChangePasswordAtLogon $true -ErrorAction Stop
-                    Write-Host "Temporary password set to $temporaryPassword. User must change the password at the next login."
-                } catch {
-                    Write-Host "Error: $_"
-                }
-                Write-Host "Press Enter to continue"
-                Read-Host
-            }
+}
             '4' {
+                # Asset Control submenu
+                Asset-Control -userId $userId
+            }
+            '5' {
                 # Quit the script
                 return
             }
