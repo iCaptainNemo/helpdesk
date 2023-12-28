b1bef7e29b52f19d912a774094a08dfffde0f66c
iCaptainNemo <m.gfilippi@gmail.com>
2023-12-27 16:57:44 -0800

    fixed: test asset connection function to work with $computername varianle properly

--- helpdesk_jarvis.ps1
+++ helpdesk_jarvis.ps1
@@ -183,11 +183,12 @@ function Unlock-ADAccountOnAllDomainControllers {
 # Function to test connection to an asset
 function Test-AssetConnection {
     param (
-        [string]$assetName
+        [Parameter(Mandatory=$true)]
+        [string]$ComputerName
     )
 
     try {
-        $null = Test-Connection -ComputerName $assetName -Count 1 -ErrorAction Stop
+        $null = Test-Connection -ComputerName $ComputerName -Count 1 -ErrorAction Stop
         return $true
     } catch {
         return $false
