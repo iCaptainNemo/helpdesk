bfd5c31f3099b320d8067af0f017d486c332c976
iCaptainNemo <m.gfilippi@gmail.com>
2024-01-18 17:07:40 -0800

    Refactor Unlock-ADAccountOnAllDomainControllers function

--- Unlocker.ps1
+++ Unlocker.ps1
@@ -7,7 +7,6 @@ Import-Module ActiveDirectory
 # Get the current user with specific properties
 $AdminUser = Get-ADUser -Identity $env:USERNAME -Properties SamAccountName, Name
 
-
 # Get the current domain
 $currentDomain = (Get-ADDomain).DNSRoot
 Write-Host "Current domain: $currentDomain"
@@ -17,7 +16,6 @@ function Get-CurrentTime {
 
 # Import variables from env.ps1 file
 . .\env_$currentDomain.ps1
-
 
 $unlockedUsersCount = 0
 
@@ -61,47 +59,36 @@ function Unlock-ADAccountOnAllDomainControllers {
         }
     }
 }
-
-
 $unlockedUsersCount = 0
 # Function to get probable locked-out users
 function Get-ProbableLockedOutUsers {
-# Search for all locked-out user accounts
-$lockedOutUsers = Search-ADAccount -LockedOut -UsersOnly
+    # Search for all locked-out user accounts
+    $lockedOutUsers = Search-ADAccount -LockedOut -UsersOnly
 
-# Iterate through all locked-out users and get additional AD properties
-$probableLockedOutUsers = foreach ($lockedOutUser in $lockedOutUsers) {
-    Get-ADUser -Identity $lockedOutUser.SamAccountName -Properties *
-        }
+    # Iterate through all locked-out users and get additional AD properties
+    $probableLockedOutUsers = foreach ($lockedOutUser in $lockedOutUsers) {
+        Get-ADUser -Identity $lockedOutUser.SamAccountName -Properties *
+    }
 
-        # Filter locked-out users whose lockoutTime is within X days of the current date, Enabled is True, PasswordExpired is False, and badPwdCount is greater than 0
-        $probableLockedOutUsers = $probableLockedOutUsers | Where-Object {
-            $_.AccountlockoutTime -ge (Get-Date).AddDays(-1) -and
-            $_.Enabled -eq $true
-        }
-        # Users who are locked out and password is expired
-        $lockedoutusersB = $probableLockedOutUsers | Where-Object {
-            #$_.LockedOut -eq $true -and
-            $_.PasswordExpired -eq $true
-        }
+    # Filter locked-out users whose lockoutTime is within X days of the current date, Enabled is True, PasswordExpired is False, and badPwdCount is greater than 0
+    $probableLockedOutUsers = $probableLockedOutUsers | Where-Object {
+        $_.AccountlockoutTime -ge (Get-Date).AddDays(-1) -and
+        $_.Enabled -eq $true
+    }
+    # Users who are locked out and password is expired
+    $lockedoutusersB = $probableLockedOutUsers | Where-Object {
+        $_.PasswordExpired -eq $true
+    }
 
-        # Users who are locked out and badPwdCount is 0 or null
-        $lockedoutusersC = $probableLockedOutUsers | Where-Object {
-            #$_.LockedOut -eq $true -and
-            ($_.badPwdCount -lt 3 -or $_.badPwdCount -eq $null)
-        }
-
-        # The rest of the users
-        $lockedoutusersA = $probableLockedOutUsers | Where-Object {
-            $_ -notin $lockedoutusersB -and
-            $_ -notin $lockedoutusersC
-        }
-        return @{
-            'ProbableLockedOutUsers' = $probableLockedOutUsers
-            'LockedOutUsersA' = $lockedoutusersA
-            'LockedOutUsersB' = $lockedoutusersB
-            'LockedOutUsersC' = $lockedoutusersC
-        }
+    # The rest of the users
+    $lockedoutusersA = $probableLockedOutUsers | Where-Object {
+        $_ -notin $lockedoutusersB
+    }
+    return @{
+        'ProbableLockedOutUsers' = $probableLockedOutUsers
+        'LockedOutUsersA' = $lockedoutusersA
+        'LockedOutUsersB' = $lockedoutusersB
+    }
 }
 
 function Unlock-Users {
@@ -176,40 +163,32 @@ $restartScript = $true
 
 while ($restartScript) {
     Clear-Host
-     # Display the current time
-     $currentTime = Get-CurrentTime
-     Write-Host "Current Time: $currentTime"
-     Write-Host "Unlocked Users Count: $unlockedCount"  -ForegroundColor Green
+    # Display the current time
+    $currentTime = Get-CurrentTime
+    Write-Host "Current Time: $currentTime"
+    Write-Host "Unlocked Users Count: $unlockedCount"  -ForegroundColor Green
 
     #Line break for space
     Write-Host "`n"
-    
 
     # Get probable locked-out users
     $result = Get-ProbableLockedOutUsers
     $probableLockedOutUsers = $result.ProbableLockedOutUsers
     $lockedoutusersA = $result.LockedOutUsersA
     $lockedoutusersB = $result.LockedOutUsersB
-    $lockedoutusersC = $result.LockedOutUsersC
 
-    # Display the properties of users in $lockedoutusersA, $lockedoutusersB, and $lockedoutusersC in separate tables
+    # Display the properties of users in $lockedoutusersA, $lockedoutusersB in separate tables
     $noUsersToUnlock = $true
 
     if ($lockedoutusersA.Count -gt 0) {
         Write-Host "Locked-out users within the last 24 hours:"
-        $lockedoutusersA | Sort-Object AccountLockoutTime -Descending | Format-Table -Property SamAccountName, Name, badPwdCount, AccountLockoutTime -AutoSize
+        $lockedoutusersA | Sort-Object AccountLockoutTime -Descending | Format-Table -Property SamAccountName, Name, AccountLockoutTime -AutoSize
         $noUsersToUnlock = $false
     }
     
     if ($lockedoutusersB.Count -gt 0) {
         Write-Host "Locked-out users Password Expired within the last 24 hours:"
-        $lockedoutusersB | Sort-Object AccountLockoutTime -Descending | Format-Table -Property SamAccountName, Name, badPwdCount, AccountLockoutTime -AutoSize
-        $noUsersToUnlock = $false
-    }
-    
-    if ($lockedoutusersC.Count -gt 0) {
-        Write-Host "Locked-out users Bad password attempts < 3 within the last 24 hours:"
-        $lockedoutusersC | Sort-Object AccountLockoutTime -Descending | Format-Table -Property SamAccountName, Name, badPwdCount, AccountLockoutTime -AutoSize
+        $lockedoutusersB | Sort-Object AccountLockoutTime -Descending | Format-Table -Property SamAccountName, Name, AccountLockoutTime -AutoSize
         $noUsersToUnlock = $false
     }
     
@@ -217,16 +196,13 @@ while ($restartScript) {
         Write-Host "No users to unlock" -ForegroundColor Green
     }
 
-     #Line break for space
-     Write-Host "`n"
+    #Line break for space
+    Write-Host "`n"
 
     # Display the menu for unlocking accounts
     Write-Host "Unlock Account Menu:"
     Write-Host "1. Unlock All Users"
     Write-Host "2. Unlock All With Password Expired Only"
-    Write-Host "3. Unlock Users BP < 3"
-    Write-Host "4. Auto Unlock Users With Password Expired"
-    Write-Host "5. Auto Unlock Users BP < 3"
 
     $choice = Read-Host "Select an option"
 
@@ -243,70 +219,5 @@ while ($restartScript) {
             #Clear-Host
             $unlockedCount += Unlock-Users -lockedoutusers $lockedoutusersB
         }
-        3 {
-            #Unlock all users with bad password count = 0 or Null
-            #Clear-Host
-            $unlockedCount += Unlock-Users -lockedoutusers $lockedoutusersC
-        }
-        4 {
-            # Auto Unlock Users With Password Expired
-            # Prompt user for refresh interval
-            do {
-                $refreshInterval = Read-Host "Enter the refresh interval in minutes (e.g., 1, 5, 10):"
-                $refreshInterval = [int]$refreshInterval
-            } while ($refreshInterval -le 0)
-
-            
-            # Get probable locked-out users
-            $result = Get-ProbableLockedOutUsers
-            $probableLockedOutUsers = $result.ProbableLockedOutUsers
-            $lockedoutusersA = $result.LockedOutUsersA
-            $lockedoutusersB = $result.LockedOutUsersB
-
-            # Auto Unlock at specified refresh interval
-            do {
-                # Clear-Host
-                # Write-Host "Auto Unlocking every $refreshInterval minutes. Press Ctrl+C to stop."
-                Start-Sleep -Seconds (60 * $refreshInterval)
-
-               if ($lockedoutusersB -ne $null) {
-                $unlockedCount += Unlock-Users -lockedoutusers $lockedoutusersB
-            } else {
-                Write-Host "No Users to unlock"
-            }
-            } while ($true)
-        }
-        5 {
-            # Auto Unlock Users BP < 3
-            # Prompt user for refresh interval
-            do {
-                $refreshInterval = Read-Host "Enter the refresh interval in minutes (e.g., 1, 5, 10):"
-                $refreshInterval = [int]$refreshInterval
-            } while ($refreshInterval -le 0)
-
-            # Get probable locked-out users
-            $result = Get-ProbableLockedOutUsers
-            $probableLockedOutUsers = $result.ProbableLockedOutUsers
-            $lockedoutusersA = $result.LockedOutUsersA
-            $lockedoutusersB = $result.LockedOutUsersB
-
-            # Auto Unlock at specified refresh interval
-            do {
-                # Clear-Host
-                # Write-Host "Auto Unlocking every $refreshInterval minutes. Press Ctrl+C to stop."
-                Start-Sleep -Seconds (60 * $refreshInterval)
-                
-                $lockedoutusersC = $result.LockedOutUsersC
-                if ($lockedoutusersC -ne $null) {
-                    $unlockedCount += Unlock-Users -lockedoutusers $lockedoutusersC
-                } else {
-                    Write-Host "No Users to unlock"
-                }
-            } while ($true)
-
-        }
     }
-
-        # Add a restart message outside the loop
-        Write-Host "Restarting the script..."
 }
