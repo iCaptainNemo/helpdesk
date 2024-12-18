8b8163d8251eecaf3bc0ea5e63cd983dd2074915
iCaptainNemo <m.gfilippi@gmail.com>
2024-12-18 13:57:49 -0800

    adjust dashboard layout for improved alignment and spacing

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -12,35 +12,8 @@ const Dashboard = () => {
                 <ServerStatus />
                 <DomainControllers /> {/* Add the new component */}
             </div>
-            <form id="helloWorldForm" onSubmit={handleHelloWorldButtonClick}>
-                <button type="submit">Test</button>
-            </form>
-            <div id="helloWorldOutputContainer"></div>
         </div>
     );
-};
-
-const handleHelloWorldButtonClick = async (event) => {
-    event.preventDefault();
-    try {
-        const response = await fetch('/api/hello-world', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${localStorage.getItem('token')}` // Include token in Authorization header
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
-        const data = await response.json();
-        document.getElementById('helloWorldOutputContainer').innerText = data.message;
-    } catch (error) {
-        console.error('Error fetching hello world output:', error);
-        document.getElementById('helloWorldOutputContainer').innerText = 'Error fetching hello world output';
-    }
 };
 
 export default Dashboard;
