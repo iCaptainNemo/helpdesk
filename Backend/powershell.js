fba1e418482396ddea72763b3943bb4b3ac647f8
iCaptainNemo <m.gfilippi@gmail.com>
2025-01-13 09:58:27 -0800

    Uncomment LockedOutList.ps1 in scriptsToSuppressLogging to enable logging for better debugging

--- Backend/powershell.js
+++ Backend/powershell.js
@@ -3,7 +3,7 @@ const { log, info, warn, error, verbose, debug } = require('./utils/logger');
 
 // List of scripts where stdout logging should be suppressed
 const scriptsToSuppressLogging = [
-    'LockedOutList.ps1',
+//    'LockedOutList.ps1',
     'getDomainInfo.ps1',
     'Get-ADObject.ps1',
     'logFilePath.ps1',
